925198344380c47908b259fb792bbf4f75f40cfc
Fariba <fariba.ghorbani2127@gmail.com>
2023-06-06 02:51:15 +0330

    feat: implement schema of izhikevich behavior
    at this point we don't get any error but the correct plot doesn't
    show up.

--- Projects/P01/izhikevich.py
+++ Projects/P01/izhikevich.py
@@ -1,4 +1,5 @@
 from pymonntorch import *
+import torch
 
 class Izhikevich(Behavior):
     def initialize(self, neurons):
@@ -7,9 +8,24 @@ class Izhikevich(Behavior):
         self.b = self.parameter("b", None)
         self.c = self.parameter("c", None)
         self.d = self.parameter("d", None)
-        self.u = self.parameter("u", None)
-        self.v = self.parameter("v", None)
-        self.u_rest = self.parameter("u_rest", None)
+        self.v_rest = self.parameter("v_rest", None)
         self.threshold = 30
 
-    # def forward(self, neurons):
+        neurons.u = neurons.vector("zeros")
+        neurons.v = neurons.vector(self.v_rest)
+
+    def forward(self, neurons):
+        deriv_v = (torch.pow(neurons.v, 2) * 0.04) + neurons.v * 5 + 140 - neurons.u + neurons.I
+        deriv_u = self.a * (self.b * neurons.v - neurons.u)
+
+        neurons.v = neurons.v + torch.mul(neurons.v, deriv_v) * neurons.network.dt
+        neurons.u = neurons.u + torch.mul(neurons.u, deriv_u) * neurons.network.dt
+
+        # finding the neurons which reach the threshold
+        neurons.spike = neurons.v > self.threshold
+        
+        # changing the parameters according to the izhikevich condition
+        neurons.v[neurons.spike] = self.c
+        neurons.u[neurons.spike] = neurons.u[neurons.spike] + self.d
+
+ 
--- Projects/P01/main.py
+++ Projects/P01/main.py
@@ -3,6 +3,8 @@ from time_resolution import TimeResolution
 import torch
 from izhikevich import Izhikevich
 from constant_current import ConstantCurrent
+import matplotlib.pyplot as plt
+
 
 settings = {"def_type": torch.float32, "device": 'cpu', "synapse_mode": SxD}
 net = Network(behavior={1: TimeResolution(dt=1)}, settings=settings)
@@ -14,8 +16,13 @@ neuron_pop1 = NeuronGroup(size=1,
                                                   b=0.2,
                                                   c=-66,
                                                   d=8,
-                                                  v_rest=-70,
-                                                  v=0,
-                                                  u=0),
-                                    9: Recorder(tag="neuron_pop1", variables=["u"])
+                                                  v_rest=-55,
+                                                  ),
+                                    9: Recorder(tag="neuron_pop1", variables=["v"])
                                     })
+
+net.initialize()
+net.simulate_iterations(100)
+
+plt.plot(net["neuron_pop1", 0].variables["v"][:,:])
+plt.show()
