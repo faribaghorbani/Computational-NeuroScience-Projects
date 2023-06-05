dc611246d9b37b7585f1e36b08c85c8a74d180c1
Fariba <fariba.ghorbani2127@gmail.com>
2023-06-06 00:09:48 +0330

    feat: add the neuron group and behaviors
    the neuron group object is created and the hierarchy of the behaviors
    are added such as:
    - constant current
    - izhikevich
    - recorder

--- Projects/P01/main.py
+++ Projects/P01/main.py
@@ -1,6 +1,21 @@
 from pymonntorch import *
 from time_resolution import TimeResolution 
 import torch
+from izhikevich import Izhikevich
+from constant_current import ConstantCurrent
 
 settings = {"def_type": torch.float32, "device": 'cpu', "synapse_mode": SxD}
 net = Network(behavior={1: TimeResolution(dt=1)}, settings=settings)
+
+neuron_pop1 = NeuronGroup(size=1,
+                          net=net,
+                          behavior={1: ConstantCurrent(i=10),
+                                    3: Izhikevich(a=0.02,
+                                                  b=0.2,
+                                                  c=-66,
+                                                  d=8,
+                                                  v_rest=-70,
+                                                  v=0,
+                                                  u=0),
+                                    9: Recorder(tag="neuron_pop1", variables=["u"])
+                                    })
