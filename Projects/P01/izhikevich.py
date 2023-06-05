b72c4b4d1bf2af93ef8d5a2ef8342fa1d38f01bb
Fariba <fariba.ghorbani2127@gmail.com>
2023-06-06 03:14:27 +0330

    fix: debugging izhikevich implementation
    Euler formula in izhikevich behavior class has been modified

--- Projects/P01/izhikevich.py
+++ Projects/P01/izhikevich.py
@@ -14,18 +14,22 @@ class Izhikevich(Behavior):
         neurons.u = neurons.vector("zeros")
         neurons.v = neurons.vector(self.v_rest)
 
+
     def forward(self, neurons):
-        deriv_v = (torch.pow(neurons.v, 2) * 0.04) + neurons.v * 5 + 140 - neurons.u + neurons.I
+        deriv_v = (torch.pow(neurons.v, 2) * 0.04) + (neurons.v * 5) + 140 - neurons.u + neurons.I
         deriv_u = self.a * (self.b * neurons.v - neurons.u)
 
-        neurons.v = neurons.v + torch.mul(neurons.v, deriv_v) * neurons.network.dt
-        neurons.u = neurons.u + torch.mul(neurons.u, deriv_u) * neurons.network.dt
+        neurons.v = neurons.v + deriv_v * neurons.network.dt
+        neurons.u = neurons.u + deriv_u * neurons.network.dt
 
         # finding the neurons which reach the threshold
         neurons.spike = neurons.v > self.threshold
         
         # changing the parameters according to the izhikevich condition
         neurons.v[neurons.spike] = self.c
-        neurons.u[neurons.spike] = neurons.u[neurons.spike] + self.d
+        neurons.u[neurons.spike] = neurons.u[neurons.spike] + neurons.vector(self.d)
+
+        print(neurons.v)
+        print(neurons.u)
 
  
--- Projects/P01/main.py
+++ Projects/P01/main.py
@@ -14,15 +14,15 @@ neuron_pop1 = NeuronGroup(size=1,
                           behavior={1: ConstantCurrent(i=10),
                                     3: Izhikevich(a=0.02,
                                                   b=0.2,
-                                                  c=-66,
-                                                  d=8,
+                                                  c=-50,
+                                                  d=2,
                                                   v_rest=-55,
                                                   ),
                                     9: Recorder(tag="neuron_pop1", variables=["v"])
                                     })
 
 net.initialize()
-net.simulate_iterations(100)
+net.simulate_iterations(300)
 
 plt.plot(net["neuron_pop1", 0].variables["v"][:,:])
 plt.show()
